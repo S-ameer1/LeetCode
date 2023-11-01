bb075276dad728bb22edd20cecb05314f1ac71a3
S-ameer1 <100997554+S-ameer1@users.noreply.github.com>
2023-11-01 22:37:14 +0530

    Time: 15 ms (69.51%), Space: 26 MB (19.38%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -1,44 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    unordered_map<int,int>map;
-
-    void help(TreeNode*root,vector<int>&ans){
-        if(root==NULL){
-            return ;
-        }
-        ans.push_back(root->val);
-        help(root->left,ans);
-        help(root->right,ans);
-    }
     vector<int> findMode(TreeNode* root) {
-        vector<int>ans;
-        help(root,ans);
-        int max=INT_MIN;
-        for(int i=0;i<ans.size();i++){
-            map[ans[i]]++;
-            if(map[ans[i]]>max){
-                max=map[ans[i]];
+        vector<int> inorderr;
+        inorder(root, inorderr);
+        unordered_map<int, int> freq;
+        int maxx = 0;
+        
+        for (int val : inorderr) {
+            freq[val]++;
+            maxx = max(maxx, freq[val]);
+        }
+        
+        vector<int> result;
+        for (const auto& pair : freq) {
+            if (pair.second == maxx) {
+                result.push_back(pair.first);
             }
         }
-        vector<int>finalans;
-        for(auto i:map){
-            if(i.second==max){
-                finalans.push_back(i.first);
-            }
-        }
+        
+        return result;
+    }
 
-
-        return finalans;
+private:
+    void inorder(TreeNode* root, vector<int>& result) {
+        if (!root) return;
+        result.push_back(root->val);
+        inorder(root->left, result);
+        inorder(root->right, result);
     }
 };
