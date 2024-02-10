9772393ea92d4b7e7bc6481a1614dce2f5c20dd0
S-ameer1 <100997554+S-ameer1@users.noreply.github.com>
2024-02-10 14:47:13 +0530

    Time: 2 ms (86.45%), Space: 7.5 MB (69.49%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence24-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitter-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divexpedia-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div647-palindromic-substrings/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence24-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitter-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divexpedia-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div647-palindromic-substrings.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence24-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitter-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divexpedia-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div647-palindromic-substrings/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence24-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitter-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divexpedia-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div647-palindromic-substrings.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    bool ispalind(string temp,vector<vector<int>>&dp,int i,int j){
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int s=0;
-        int e=temp.size()-1;
-        while(s<=e){
-            if(temp[s]!=temp[e]){
-                return dp[i][j]=0;
-            }
-            s++;
-            e--;
-        }
-        return dp[i][j]=1;
-    }
+
     int countSubstrings(string s) {
+        int count1=0;
+        int count2=0;
         int n=s.size();
-        int count=0;
-        vector<vector<int>>dp(n,vector<int>(n,-1));
         for(int i=0;i<n;i++){
-            string temp="";
-            for(int j=i;j<n;j++){
-                temp+=s[j];
-                count+=ispalind(temp,dp,i,j);
+            int l=i;
+            int r=i+1;
+            while(l>=0 && r<n && s[l]==s[r]){
+                count1++;
+                l--;
+                r++;
             }
         }
-        return count;
+        for(int i=0;i<n;i++){
+            int l=i;
+            int r=i;
+            while(l>=0 && r<n && s[l]==s[r]){
+                count2++;
+                l--;
+                r++;
+            }
+        }
+        return count1+count2;
     }
 };
